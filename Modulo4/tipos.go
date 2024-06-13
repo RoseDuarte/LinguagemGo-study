092d90e1aa872b0df2ffa9d3b9c1c980240bc5fd
Roselena Duarte <roseduartte7@gmail.com>
2024-06-13 15:45:52 -0300

    Adição e alteração
    Adição da pasta Modulo4 (tipos, varconst e zerovalues) e alteração em pacotes.

---
+++ Modulo4/tipos.go
@@ -0,0 +1,27 @@
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// bool (true/false)
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+
+	//string - sequência de bytes
+	fmt.Printf("Type: %T - Value: %v\n", "code", "code")
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+
+	//int
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	
+	//float (float64/float32) - decimal
+	fmt.Printf("TYpe: %T - Value: %v\n", 1.233, 1.233)
+}
+
+
+
+
+
+ 
---
+++ Modulo4/varconst.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variáveis
+	// var + nome da variavel + tipo
+	var nome string
+	nome = "bento"
+	fmt.Println(nome)
+
+	nome = "maria"
+	fmt.Println(nome)
+
+	var  idade int
+	idade = 4
+	fmt.Println(idade)
+
+	var a = "maria"
+	fmt. Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	//forma abreviada
+	f := "apple"
+	fmt.Println(f)
+
+	// Constantes
+	const idadeBento = 4
+	fmt.Println(idadeBento)
+}
+
+
+
